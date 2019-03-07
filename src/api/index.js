33bbb2e790daacceffbc8a214a8c8ab32e8a1590
unknown <xurask8@gmail.com>
2019-03-07 02:42:07 -0300

    Upando para debug

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import Axios from 'axios';
 
 export default Axios.create({
-  baseURL: 'http://caserahost.ddns.net'
+  baseURL: 'http://caserahost.ddns.net',
 });
--- src/components/Register2.js
+++ src/components/Register2.js
@@ -1,30 +1,26 @@
-import React from 'react';
-
-import {
-  AppRegistry,
-  Image,
-  PixelRatio,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import React, { Component } from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+import { connect } from 'react-redux'
+import { bindActionCreators } from "redux";
 import ImagePicker from 'react-native-image-picker';
 
-export default class App extends React.Component {
-  state = {
-    avatarSource: null,
-    videoSource: null,
-  };
+import * as Actions from '../redux/actions';
+import api from '../api';
 
-  constructor(props) {
-    super(props);
-
-    this.selectPhotoTapped = this.selectPhotoTapped.bind(this);
-    this.selectVideoTapped = this.selectVideoTapped.bind(this);
+export class Register2 extends Component {
+  static navigationOptions = {
+    header: null
   }
 
-  selectPhotoTapped() {
+  constructor() {
+    super()
+
+    this.state = {
+      imageUri: ''
+    }
+  }
+
+  selectImage() {
     const options = {
       quality: 1.0,
       maxWidth: 500,
@@ -34,104 +30,50 @@ export default class App extends React.Component {
       },
     };
 
-    ImagePicker.showImagePicker(options, (response) => {
-      console.log('Response = ', response);
-
-      if (response.didCancel) {
-        console.log('User cancelled photo picker');
-      } else if (response.error) {
-        console.log('ImagePicker Error: ', response.error);
-      } else if (response.customButton) {
-        console.log('User tapped custom button: ', response.customButton);
-      } else {
-        let source = { uri: response.uri };
-
-        // You can also display the image using data:
-        // let source = { uri: 'data:image/jpeg;base64,' + response.data };
-
-        this.setState({
-          avatarSource: source,
-        });
+    const header = {
+      headers: {
+        'Accept': '',
+        'Content-Type': 'multipart/form-data',
+        'authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVjN2UzYjZmNzMyODA5MjcxMGIwNjgxNyIsImlhdCI6MTU1MTg2NjIzNiwiZXhwIjoxNTUxOTUyNjM2fQ.QNiGB6zssrAZZNLD8c_vPIyl_3tbfmxxLeXnhqV66l4'
       }
-    });
-  }
+    }
 
-  selectVideoTapped() {
-    const options = {
-      title: 'Video Picker',
-      takePhotoButtonTitle: 'Take Video...',
-      mediaType: 'video',
-      videoQuality: 'medium',
-    };
+    ImagePicker.showImagePicker(options, response => {
+      let data = new FormData()
 
-    ImagePicker.showImagePicker(options, (response) => {
-      console.log('Response = ', response);
+      data.append('file', {
+        name: response.fileName,
+        type: response.type,
+        uri: response.uri,
+      })
 
-      if (response.didCancel) {
-        console.log('User cancelled video picker');
-      } else if (response.error) {
-        console.log('ImagePicker Error: ', response.error);
-      } else if (response.customButton) {
-        console.log('User tapped custom button: ', response.customButton);
-      } else {
-        this.setState({
-          videoSource: response.uri,
-        });
-      }
+      api.patch('/users/profilePhoto/5c7fbc0bede89f1fb04a23bc', data, header).then((response) => {
+        console.log('OKOK');
+
+      }).catch(err => {
+        console.log(err.response);
+      });
     });
   }
 
   render() {
+    console.disableYellowBox = true;
     return (
-      <View style={styles.container}>
-        <TouchableOpacity onPress={this.selectPhotoTapped.bind(this)}>
-          <View
-            style={[
-              styles.avatar,
-              styles.avatarContainer,
-              { marginBottom: 20 },
-            ]}
-          >
-            {this.state.avatarSource === null ? (
-              <Text>Select a Photo</Text>
-            ) : (
-                <Image style={styles.avatar} source={this.state.avatarSource} />
-              )}
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <TouchableOpacity onPress={this.selectImage.bind(this)} >
+          <View style={{ width: 200, height: 200, alignItems: 'center', justifyContent: 'center', backgroundColor: '#DDD' }}>
+            <Text>Select Image</Text>
           </View>
         </TouchableOpacity>
-
-        <TouchableOpacity onPress={this.selectVideoTapped.bind(this)}>
-          <View style={[styles.avatar, styles.avatarContainer]}>
-            <Text>Select a Video</Text>
-          </View>
-        </TouchableOpacity>
-
-        {this.state.videoSource && (
-          <Text style={{ margin: 8, textAlign: 'center' }}>
-            {this.state.videoSource}
-          </Text>
-        )}
       </View>
-    );
+    )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  avatarContainer: {
-    borderColor: '#9B9B9B',
-    borderWidth: 1 / PixelRatio.get(),
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  avatar: {
-    borderRadius: 75,
-    width: 150,
-    height: 150,
-  },
-});
+const mapStateToProps = (state) => ({
+  account: state.account
+})
+
+const mapDispatchToProps = dispatch => bindActionCreators(Actions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Register2)
